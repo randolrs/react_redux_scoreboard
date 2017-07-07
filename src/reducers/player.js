10b519debd8125c6870b6f35185e0892958cd472
randolrs <r.shane.randolph@gmail.com>
2017-07-07 15:21:41 -0400

    initial commit

---
+++ src/reducers/player.js
@@ -0,0 +1,86 @@
+import * as PlayerActionTypes from '../actiontypes/player';
+
+const initialState = {
+	players: [{
+		name: 'Shane Randolph',
+	  score: 31,
+		created: '7/4/2017',
+		updated: '7/5/2017'
+	},
+	{
+		name: 'Elise Noel',
+		score: 20,
+		created: '7/5/2017',
+		updated: '7/6/2017'
+	},
+	{
+		name: 'Jordan Randolph',
+		score: 50,
+		created: '7/6/2017',
+		updated: '7/7/2017'
+	}
+	],
+	selectedPlayerIndex: -1
+}
+
+export default function Player(state=initialState, action) {
+  
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth()+1;
+  let year = date.getFullYear();
+
+
+  switch(action.type) {
+    case PlayerActionTypes.ADD_PLAYER: {
+      const addPlayerList = [...state.players,  {
+          name: action.name,
+          score: 0,
+          created: `${month}/${day}/${year}`
+      }];
+      return {
+        ...state,
+        players: addPlayerList
+      };
+    }
+
+    case PlayerActionTypes.REMOVE_PLAYER: {
+      const removePlayerList = [
+        ...state.players.slice(0, action.index),
+        ...state.players.slice(action.index + 1)
+      ];
+
+      return {
+          ...state,
+          players: removePlayerList
+      };
+    }
+      
+    case PlayerActionTypes.UPDATE_PLAYER_SCORE: {
+      const updatePlayerList = state.players.map((player, index) => {
+        if(index === action.index) {
+          return {
+            ...player,
+            score: player.score + action.score,
+            updated: `${month}/${day}/${year}`
+          };
+        }
+        return player;
+      });
+      return {
+        ...state,
+        players: updatePlayerList
+      };
+    }
+
+    case PlayerActionTypes.SELECT_PLAYER:{
+      return {
+        ...state,
+        selectedPlayerIndex: action.index
+      };
+    }
+      
+    default:
+      return state;
+  }
+}
